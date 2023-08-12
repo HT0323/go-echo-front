71ed283c87527370f036a5e4cee570be44d5abc9
HaraT0323 <alra9si916323@gmail.com>
2023-08-12 22:16:23 +0900

    認証コンポーネントの実装

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,24 @@
+import { useQueryClient } from '@tanstack/react-query'
+import {
+  ArrowRightOnRectangleIcon,
+} from '@heroicons/react/24/solid'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
 export const Todo = () => {
+  const queryClient = useQueryClient()
+  const { logoutMutation } = useMutateAuth()
+
+  const logout = async () => {
+    await logoutMutation.mutateAsync()
+    queryClient.removeQueries(['tasks'])
+  }
   return (
-    <div>Todo</div>
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+
+      <ArrowRightOnRectangleIcon
+        onClick={logout}
+        className="h-6 w-6 my-6 text-blue-500 cursor-pointer"
+      />
+    </div>
   )
 }
